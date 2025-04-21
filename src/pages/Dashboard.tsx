a5c3dc9d9a1fdf6ab435a76c515994815b3e277b
Ferhat Cansev <fsunaycansev@gmail.com>
2025-04-21 16:23:16 +0300

    chore: setup tailwindcss and add reset css

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,7 @@
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-red-500">Dashboard</h1>
+    </div>
+  );
 };
