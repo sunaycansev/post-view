abe5489df97c4e7a9d7b60abff7312b7b011cea3
Ferhat Cansev <fsunaycansev@gmail.com>
2025-04-21 22:41:46 +0300

    chore: setup tanstack/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,36 @@ import "./App.css";
 import routes from "@/routes/routes";
 import AuthWrapper from "@/components/AuthWrapper";
 import PrefetchWrapper from "@/components/PrefetchWrapper";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {routes.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={
-              <AuthWrapper
-                isPrivate={route.isPrivate}
-                permissions={route.permissions}
-              >
-                <PrefetchWrapper translationKeys={route.translations}>
-                  <route.renderer />
-                </PrefetchWrapper>
-              </AuthWrapper>
-            }
-          />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <AuthWrapper
+                  isPrivate={route.isPrivate}
+                  permissions={route.permissions}
+                >
+                  <PrefetchWrapper translationKeys={route.translations}>
+                    <route.renderer />
+                  </PrefetchWrapper>
+                </AuthWrapper>
+              }
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
